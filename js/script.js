0f2ffc95614d4d228db1cdc25abfb290b6c58bd6
dokkalemon <dottori.roberto@gmail.com>
2023-01-25 23:18:33 +0100

    refactor header

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-alert('buona sera')
+const headerRef = document.querySelector('header');
+
+const scrollWindow = () => {
+    window.scrollY > 100 ?
+        headerRef.classList.add('scrolled') : headerRef.classList.remove('scrolled')
+
+}
+
+window.addEventListener('scroll', scrollWindow)
+
+
