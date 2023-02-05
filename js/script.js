f0253a4131ace4ae69dad92e43d05969c1d189db
dokkalemon <roberto.dottori@nautes.com>
2023-02-05 21:22:23 +0100

    updates

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
-const headerRef = document.querySelector('header');
-
+const headerRef = document.querySelector("header");
+const buttonUpRef = document.querySelector(".back-up-container");
 const scrollWindow = () => {
-    window.scrollY > 100 ?
-        headerRef.classList.add('scrolled') : headerRef.classList.remove('scrolled')
+  window.scrollY > 200
+    ? headerRef.classList.add("scrolled")
+    : headerRef.classList.remove("scrolled");
 
-}
+  window.scrollY > 500
+    ? buttonUpRef.classList.add("showed")
+    : buttonUpRef.classList.remove("showed");
+};
 
-window.addEventListener('scroll', scrollWindow)
-
-
+window.addEventListener("scroll", scrollWindow);
