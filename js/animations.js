eeb632c1594e1357d80617356d2c0cd2073f1250
dokkalemon <dottori.roberto@gmail.com>
2023-02-15 22:40:33 +0100

    add animations

--- js/animations.js
+++ js/animations.js
@@ -142,3 +142,70 @@ gsap.fromTo(cards[2], {
         start: 'top center',
     }
 })
+
+gsap.fromTo('#appointment h3', {
+    x: -100,
+    opacity: 0,
+    duration: 1,
+}, {
+    x: 0,
+    opacity: 1,
+    scrollTrigger: {
+        trigger: '.appointment',
+        start: 'top center',
+    }
+})
+gsap.fromTo('#appointment p', {
+    x: -100,
+    opacity: 0,
+    duration: 1,
+}, {
+    x: 0,
+    opacity: 1,
+    delay: 0.5,
+    scrollTrigger: {
+        trigger: '.appointment',
+        start: 'top center',
+    }
+})
+
+gsap.fromTo('#appointment h4', {
+    x: -100,
+    opacity: 0,
+    duration: 1,
+}, {
+    x: 0,
+    opacity: 1,
+    delay: 0.5,
+    scrollTrigger: {
+        trigger: '.appointment',
+        start: 'top center',
+    }
+})
+
+gsap.fromTo('form', {
+    x: 100,
+    opacity: 0,
+    duration: 1,
+}, {
+    x: 0,
+    opacity: 1,
+    delay: 0.5,
+    scrollTrigger: {
+        trigger: '.appointment',
+        start: 'top center',
+    }
+})
+
+gsap.fromTo('#form-text', {
+    x: 100,
+    opacity: 0,
+    duration: 1,
+}, {
+    x: 0,
+    opacity: 1,
+    scrollTrigger: {
+        trigger: '.appointment',
+        start: 'top center',
+    }
+})
